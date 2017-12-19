b46a3a6b2e395ed567127200dea39236849c5426
Niul Panvalkar <niul.panvalkar@cuelogic.com>
2017-12-19 19:04:30 +0530

    added factorial function

--- niul.java
+++ niul.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class niul
 {
+
+public static int fac(int n)
+{
+ if(n==0)
+  {
+    return 1;
+  }
+ else
+  {
+    return n*fac(n-1);
+  }
+}
+
 public static void main(String[] args)
  { 
+     Scanner sc = new Scanner(System.in);
      System.out.println("Hello world!!");
+     System.out.println("enter the number:");
+     int n = sc.nextInt();
+     System.out.println(fac(n));
  }
 }
